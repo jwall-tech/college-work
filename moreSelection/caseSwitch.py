41a06479b8562c322129c92ab9f9e675ec70d1a7
Wallace <M2100448@middlesbro.ac.uk>
2021-11-09 12:58:37 +0000

    select

---
+++ moreSelection/caseSwitch.py
@@ -0,0 +1,10 @@
+code = "0"
+
+match code:
+    case "200":
+        print("200")
+    case "400":
+        print("400")
+
+    case _:
+        print("no")
---
+++ moreSelection/ternaryOp.py
@@ -0,0 +1,3 @@
+age = 4000
+discount = True if age >= 400414 else False
+print(discount)
